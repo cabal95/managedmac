beeba446addacad782f12cb98b78e656e6d5cdbe
Daniel Hazelbaker <daniel@blueboxmoon.com>
2014-02-27 21:43:44 -0800

    Fix build.sh and preinstall scripts.

--- build.sh
+++ build.sh
@@ -13,12 +13,19 @@ TMPROOT="/tmp/mmbuild-root.$$"
 mkdir -p "$TMPROOT"
 mkdir -p "$TMPROOT"/usr/local/managedmac
 mkdir -p "$TMPROOT"/usr/local/managedmac/mmlib
+mkdir -p "$TMPROOT"/usr/local/managedmac/plugins/actions
 mkdir -p "$TMPROOT"/Library/LaunchDaemons
 
 cp -a source/managedmac "$TMPROOT"/usr/local/managedmac
 cp -a source/mmlib/*.py "$TMPROOT"/usr/local/managedmac/mmlib
+cp -a source/plugins/actions/*.py "$TMPROOT"/usr/local/managedmac/plugins/actions
 cp -a launchd/LaunchDaemons/com.github.managedmac-auto.plist "$TMPROOT"/Library/LaunchDaemons
 
 pkgbuild --root "$TMPROOT" --identifier "$IDENTIFIER" --version "$VERSION" --install-location / --scripts package_scripts managedmac.pkg
+if [ $? -eq 0 ]; then
+  echo "Built version $VERSION"
+fi
+
 
 rm -rf "$TMPROOT"
+
